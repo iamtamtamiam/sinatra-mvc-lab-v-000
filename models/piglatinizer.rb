7b8d704df1af6be2e7fb708c5201d719abc2245e
Tam Vu <vutam_a@hotmail.com>
2019-11-19 02:02:55 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,5 +1,20 @@
 class PigLatinizer
   
+  attr_accessor :user_input_text
+  
+  def piglatinizer(user_input_text)
+    
+    @user_input_text = user_input_text
+    @words = user_input_text.split(" ")
+    result = []
+    
+    @words.each do |word|
+      if word.start_with?()
+      
+    end 
+    
+    
+  end 
   
   
 end 
